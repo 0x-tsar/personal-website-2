a3193f45cd456594e0948ac40418bd1c0bebddb4
Gustavo <contato.gustavofernandes@hotmail.com>
2022-08-05 04:02:16 -0300

    adding new page and components

---
+++ src/components/PanelArticles.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: blue;
+  color: white;
+  font-size: 2em;
+`;
+
+type Props = {};
+
+export const PanelArticles = (props: Props) => {
+  return <Container>PanelArticles</Container>;
+};
---
+++ src/pages/articles.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import type { NextPage } from "next";
+import { PanelArticles } from "../components/PanelArticles";
+
+type Props = {};
+
+const Articles: NextPage = (props: Props) => {
+  return <PanelArticles></PanelArticles>;
+};
+
+export default Articles;
