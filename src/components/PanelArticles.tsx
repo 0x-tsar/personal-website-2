7d812f28af5591ea0956f261b022b74cb1466716
Gustavo <contato.gustavofernandes@hotmail.com>
2022-08-05 04:13:39 -0300

    adding tags

--- src/components/PanelArticles.tsx
+++ src/components/PanelArticles.tsx
@@ -9,6 +9,40 @@ export const Container = styled.div`
 
 type Props = {};
 
+type ArticleType = {
+  title: string;
+  description: string;
+  picture?: string;
+  tags: string[];
+  text: string;
+};
+
+export const EachArticle = (props: ArticleType) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <p
+        style={{
+          fontSize: "15px",
+          backgroundColor: "red",
+          borderRadius: "3px",
+          maxWidth: "fit-content",
+          padding: "5px",
+        }}
+      >
+        tags..
+      </p>
+    </div>
+  );
+};
+
 export const PanelArticles = (props: Props) => {
-  return <Container>PanelArticles</Container>;
+  return (
+    <Container>
+      <EachArticle></EachArticle>
+      <EachArticle></EachArticle>
+      <EachArticle></EachArticle>
+    </Container>
+  );
 };
