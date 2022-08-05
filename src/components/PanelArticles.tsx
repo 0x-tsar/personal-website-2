ed565f4e91f0ae9deb8ecaeac6c41c923f04b102
Gustavo <contato.gustavofernandes@hotmail.com>
2022-08-05 04:25:04 -0300

    improving article

--- src/components/PanelArticles.tsx
+++ src/components/PanelArticles.tsx
@@ -13,26 +13,40 @@ type ArticleType = {
   title: string;
   description: string;
   picture?: string;
-  tags: string[];
+  tags?: string[];
   text: string;
 };
 
 export const EachArticle = (props: ArticleType) => {
   return (
-    <div>
+    <div style={{ padding: "20px", cursor: "pointer" }}>
       <h2>{props.title}</h2>
       <p>{props.description}</p>
-      <p
+      {props.tags?.map((item, key) => {
+        return (
+          <span
+            style={{
+              fontSize: "15px",
+              backgroundColor: "red",
+              borderRadius: "3px",
+              maxWidth: "fit-content",
+              padding: "5px",
+              marginLeft: "10px",
+            }}
+            key={key}
+          >
+            {item}
+          </span>
+        );
+      })}
+      <div
         style={{
-          fontSize: "15px",
-          backgroundColor: "red",
-          borderRadius: "3px",
-          maxWidth: "fit-content",
-          padding: "5px",
+          width: "100%",
+          height: "2px",
+          backgroundColor: "black",
+          marginTop: "5px",
         }}
-      >
-        tags..
-      </p>
+      ></div>
     </div>
   );
 };
@@ -40,9 +54,14 @@ export const EachArticle = (props: ArticleType) => {
 export const PanelArticles = (props: Props) => {
   return (
     <Container>
-      <EachArticle></EachArticle>
-      <EachArticle></EachArticle>
-      <EachArticle></EachArticle>
+      <EachArticle
+        title={"O que São Oracles?"}
+        description={
+          "Para que servem as Oracles no mundo Blockchain? Quais suas vantagens?"
+        }
+        tags={["Oracles", "ChainLink", "Link"]}
+        text={""}
+      ></EachArticle>
     </Container>
   );
 };
